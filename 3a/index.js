2a8340837b1c85a181c36167a933723c615bba35
Emmiina Kivelä <kivela.emmiina@gmail.com>
2025-07-27 13:05:06 +0300

    uibuild

--- 3a/index.js
+++ 3a/index.js
@@ -1,27 +1,24 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
 
-app.use(cors())
-app.use(express.json())
 app.use(express.static('dist'))
 
 let notes = [
   {
-    id: "1",
-    content: "HTML is easy",
-    important: true
+    id: '1',
+    content: 'HTML is easy',
+    important: true,
   },
   {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
+    id: '2',
+    content: 'Browser can execute only JavaScript',
+    important: false,
   },
   {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
+    id: '3',
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true,
+  },
 ]
 
 const requestLogger = (request, response, next) => {
@@ -32,12 +29,10 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
+app.use(express.json())
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
@@ -45,8 +40,7 @@ app.get('/api/notes', (request, response) => {
 
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  
+  const note = notes.find((note) => note.id === id)
 
   if (note) {
     response.json(note)
@@ -55,17 +49,9 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
   return String(maxId + 1)
 }
 
@@ -73,8 +59,8 @@ app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+    return response.status(400).json({
+      error: 'content missing',
     })
   }
 
@@ -89,13 +75,18 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter((note) => note.id !== id)
+
+  response.status(204).end()
+})
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
-
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
